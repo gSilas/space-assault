6813003b2ed634fb840d5b83c301e6bc71137d20
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2016-05-28 11:31:46 +0200

    verzögerte richtungsbewegung der drohne; added Reset() for drone.cs

--- Space Assault/Entities/Drone.cs
+++ Space Assault/Entities/Drone.cs
@@ -4,25 +4,48 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 
+/// <summary>
+///  Movement, Schießen, Health, Sterben, neu Spawnen.
+/// </summary>
 namespace Space_Assault.Entities
 {
     class Drone : AEntity
     {
+        private Vector3 _spawnPos;
         private float _turnSpeed;
         private float _moveSpeed;
-        private Vector3 _rotation;
+        private float _moveSpeedModifier;
+        private float health;
+        private float armor;
+        private bool movementWasTrue;
 
 
         public Drone(Vector3 position)
         {
+            _spawnPos = position;
             Position = position;
         }
 
         public override void Initialize()
         {
+            movementWasTrue = false;
             RotationMatrix = Matrix.Identity;
             _turnSpeed = 1.0f;
             _moveSpeed = 1.0f;
+            health = 100;
+            armor = 100;
+        }
+
+        public void Reset()
+        {
+            movementWasTrue = false;
+            RotationMatrix = Matrix.Identity;
+            _turnSpeed = 1.0f;
+            _moveSpeed = 1.0f;
+            health = 100;
+            armor = 100;
+            _moveSpeedModifier = 0;
+            Position = _spawnPos;
         }
 
         public override void LoadContent(ContentManager cm)
@@ -32,10 +55,27 @@ namespace Space_Assault.Entities
 
         public override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.W) && Position.Y <= 160)
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
-                Position -= RotationMatrix.Forward*_moveSpeed;
+                Position -= RotationMatrix.Forward * _moveSpeed;
+                movementWasTrue = true;
             }
+            else if(movementWasTrue == true)
+            {
+                if(_moveSpeedModifier < _moveSpeed)
+                {
+                    Position -= RotationMatrix.Forward * (_moveSpeed - _moveSpeedModifier);
+                    _moveSpeedModifier += 0.02f;
+                }
+                else
+                {
+                    movementWasTrue = false;
+                    _moveSpeedModifier = 0;
+                }
+            }
+
+            //TODO: health, armor update
         }
 
         public void turn(Vector3 direction)
@@ -44,12 +84,12 @@ namespace Space_Assault.Entities
 
             if (vectorDirection > 0)
             {
-                Console.WriteLine("Left turn");
+                //turn left
                 RotationMatrix *= Matrix.CreateRotationY(MathHelper.ToRadians(_turnSpeed));
             }
             else if (vectorDirection < 0)
             {
-                Console.WriteLine("Right turn");
+                //turn right
                 RotationMatrix *= Matrix.CreateRotationY(MathHelper.ToRadians(-_turnSpeed));
             }
         }
