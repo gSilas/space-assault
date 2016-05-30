11ecb058a95a373aaa9c9a700e891bad25e2b8d5
iLiptho <philipp.thoms@web.de>
2016-05-30 09:59:38 +0200

    Abstract Weapon class, Bullet not finished

---
+++ Space Assault/Entities/Weapon/AWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Space_Assault.Utils;
+
+namespace Space_Assault.Entities.Weapon
+{
+    public abstract class AWeapon
+    {
+        public String Name;
+        public int AttackSpeed;
+        public int Damage;
+        
+
+    }
+}
