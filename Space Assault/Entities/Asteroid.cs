594fb93cae8f3fc50bfb0bc16599c1a49a421806
Daniel <Daniel@THINKPAD>
2016-05-30 10:24:19 +0200

    shitty asteroidfield

--- Space Assault/Entities/Asteroid.cs
+++ Space Assault/Entities/Asteroid.cs
@@ -8,7 +8,6 @@ namespace Space_Assault.Entities
     class Asteroid : AEntity
     {
         private float _angle;
-        private bool _up;
         private Vector3 _direction;
         private float _speed;
 
@@ -16,17 +15,8 @@ namespace Space_Assault.Entities
         {
             _angle = angle;
             Position = position;
-            //kolision später
-            //worldMatrix = Matrix.CreateWorld(base.Position, Vector3.Forward, Vector3.Up) * Matrix.CreateScale(0.05f);
-
             _direction = direction;
-            _direction.Normalize();
             _speed = movespeed;
-        }
-
-        public override void Initialize()
-        {
-            _up = true;
         }
 
         public override void LoadContent(ContentManager cm)
@@ -34,20 +24,15 @@ namespace Space_Assault.Entities
             Model = cm.Load<Model>("Models/asteroid");
         }
 
-        public override void Update(GameTime gameTime)
+        public override void Initialize()
         {
-            
+            _direction.Normalize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {           
             _angle += 0.005f;
-            if (Position.Y < 1 && _up)
-                Position += new Vector3(0, 0.002f, 0);
-            else if (Position.Y < 0)
-                _up = true;
-            else
-            {
-                Position -= new Vector3(0, 0.002f, 0);
-                _up = false;
-            }
-            RotationMatrix = Matrix.Identity;
+            RotationMatrix = Matrix.CreateRotationY(_angle);
 
             Position += _direction;
         }
---
+++ Space Assault/Utils/AsteroidBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Space_Assault.Entities;
+
+namespace Space_Assault.Utils
+{
+    class AsteroidBuilder
+    {
+        private int _chunkSize;
+        private Vector3 _referencePoint;
+        private int _distance;
+        private List<Asteroid> _asteroids;
+        private ContentManager _cm;
+
+        public AsteroidBuilder(int chunkSize, int distance, Vector3 pointOfReference,ContentManager cm)
+        {
+            _chunkSize = chunkSize;
+            _distance = distance;
+            _referencePoint = pointOfReference;
+            _referencePoint.Normalize();
+            _cm = cm;
+            _asteroids = new List<Asteroid>();
+            BuildField();
+        }
+
+        private void BuildField()
+        {
+            for (int i = 0; i <= _chunkSize; i++)
+            {
+                Asteroid ast = new Asteroid(new Vector3(10*i,0,-100*i+30*i), 0,Vector3.Backward, 0.5f);
+                ast.Initialize();
+                ast.LoadContent(_cm);
+                _asteroids.Add(ast);
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (var ast in _asteroids)
+            {
+                ast.Update(gameTime);
+            }
+        }
+
+        public void Draw(Camera _camera)
+        {
+            foreach (var ast in _asteroids)
+            {
+                ast.Draw(_camera);
+            }
+        }
+    }
+}
