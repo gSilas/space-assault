e60ec46f73931df82f346f7a06ff16f549e41a4c
iLiptho <philipp.thoms@web.de>
2016-06-17 18:06:12 +0200

    Enemyship.cs ->  doesnt work properly

---
+++ Space Assault/Entities/EnemyShip.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpaceAssault.Entities.Weapon;
+using System.Collections.Generic;
+using System;
+using SpaceAssault.Utils;
+using SpaceAssault.ScreenManager;
+
+namespace SpaceAssault.Entities
+{
+    class EnemyShip : AEntity
+    {
+        private Vector3 _spawnPos;
+        private float _moveSpeedForward;
+        //private float _moveSpeedBackward;
+        //private float _turnSpeed;
+        //private float _moveSpeedModifier;
+        private Vector3 _direction= new Vector3(0,0,0);
+        private float _moveSpeedModifier;
+ 
+        public int _health;
+        private int _armor;
+
+        private AWeapon _gun;
+
+        public EnemyShip(Vector3 position)
+        {
+            _spawnPos = position;
+            Position = position;
+        }
+
+        public override void Initialize()
+        {
+            RotationMatrix = Matrix.Identity;
+            
+            _moveSpeedForward = 0.6f;
+            //_turnSpeed = 10.0f;
+            //_moveSpeedBackward = -0.5f;
+
+            _health = 100;
+            _armor = 100;
+            _gun = new RailGun();
+            _gun.Initialize();
+        }
+        public override void LoadContent()
+        {
+            Model = Global.ContentManager.Load<Model>("Models/drone");
+            _gun.LoadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            _gun.Update(gameTime);
+            Position += _direction * _moveSpeedForward;
+            //TODO: health, armor update
+        }
+        public List<Bullet> GetBulletList()
+        {
+            return _gun.ListOfBullets;
+        }
+
+        public override void Draw()
+        {
+            _gun.Draw();
+
+            foreach (var mesh in Model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                    effect.World = RotationMatrix * Matrix.CreateWorld(Position, Vector3.Forward, Vector3.Up);
+                    effect.View = Global.Camera.ViewMatrix;
+                    effect.Projection = Global.Camera.ProjectionMatrix;
+                }
+                mesh.Draw();
+            }
+
+        }
+        public void FlyVector(Vector3 direction)
+        {
+            _direction=direction;
+        }
+
+
+    }
+}
--- Space Assault/Screens/GameplayScreen.cs
+++ Space Assault/Screens/GameplayScreen.cs
@@ -24,6 +24,7 @@ namespace SpaceAssault.Screens
         private Station _station;
         private AsteroidBuilder _asteroidField;
         private Drone _drone;
+        private EnemyShip _enemyShip;
         private Texture2D _background;
         private List<Bullet> _removeBullets;
         private List<Asteroid> _removeAsteroid;
@@ -35,12 +36,14 @@ namespace SpaceAssault.Screens
             TransitionOffTime = TimeSpan.FromSeconds(0.5);
             _station = new Station(new Vector3(0, 0, 0), 0);
             _drone = new Drone(new Vector3(0, 0, 20));
+            _enemyShip= new EnemyShip(new Vector3(20,20,20));
             //_asteroidField = new AsteroidBuilder(new Vector3(500, 0, -500));
             
             _removeAsteroid = new List<Asteroid>();
             _removeBullets = new List<Bullet>();
             _drone.Initialize();
             _station.Initialize();
+            _enemyShip.Initialize();
             _asteroidField = new AsteroidBuilder(new Vector3(0,0,1));
         }
 
@@ -53,6 +56,7 @@ namespace SpaceAssault.Screens
             Global.Camera = new Camera(Global.GraphicsManager.GraphicsDevice.DisplayMode.AspectRatio, 10000f, MathHelper.ToRadians(45), 1f, new Vector3(0, 250, 250), _drone.Position, Vector3.Up);
             _station.LoadContent();
             _drone.LoadContent();
+            _enemyShip.LoadContent();
             _asteroidField.LoadContent();
             //_asteroidField = new AsteroidBuilder(new Vector3(0,0,0));
 
@@ -86,6 +90,9 @@ namespace SpaceAssault.Screens
                 //3D Model
                 _station.Update(gameTime);
                 _drone.Update(gameTime);
+                _enemyShip.Update(gameTime);
+                //_enemyShip.FlyVector(_drone.Position);
+                
                 _asteroidField.Update(gameTime);
                 Global.Camera = new Camera(Global.GraphicsManager.GraphicsDevice.DisplayMode.AspectRatio, 10000f, MathHelper.ToRadians(45), 1f, _drone.Position + new Vector3(0, 250, 250), _drone.Position, Vector3.Up);
             }
@@ -126,6 +133,7 @@ namespace SpaceAssault.Screens
 
             _station.Draw();
             _drone.Draw();
+            _enemyShip.Draw();
             _asteroidField.Draw();
 
             // If the game is transitioning on or off, fade it out to black.
