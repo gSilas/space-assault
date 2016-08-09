fdd5c41e57b158108b59c5451575a63c6b68bf62
gSilas <d.micheel@outlook.com>
2016-08-09 20:09:52 +0200

    background optimized

--- Space Assault/Utils/Background.cs
+++ Space Assault/Utils/Background.cs
@@ -11,6 +11,7 @@ namespace SpaceAssault.Utils
         private Random _rand;
         private BasicEffect _basicEffect;
         private Texture2D _tex;
+        private RasterizerState _rast;
 
         public Background()
         {
@@ -18,15 +19,17 @@ namespace SpaceAssault.Utils
             _rand = new Random();
             _tileList = new List<Tuple<Tile,int>>();
             _tex = Global.ContentManager.Load<Texture2D>("Images/star");
-            for (int x = 0; x < 50000; x += 500)
+
+            //50*50 Tiles with 1 Star / Tile
+            //StarPosition inbetween x = 1000 y = 1000
+
+            for (int x = 0; x < 50000; x += 1000)
             {
-                for (int y = 0; y < 50000; y += 500)
+                for (int y = 0; y < 50000; y += 1000)
                 {
-                    Vector2 pos = new Vector2(x, y);                 
-                    int xo = _rand.Next(0, 501);
-                    int yo = _rand.Next(0, 501);
+                    Vector2 pos = new Vector2(x + _rand.Next(0, 1001), y + _rand.Next(0, 1001));                 
                     int height = _rand.Next(1, 8) * -1000;
-                    Tile t = new Tile(pos,xo,yo,_tex);
+                    Tile t = new Tile(pos,_tex);
                     _tileList.Add(Tuple.Create<Tile,int>(t,height));
                 }
             }
@@ -34,7 +37,7 @@ namespace SpaceAssault.Utils
 
         public void Draw()
         {
-            
+            Global.BackgroundBatch.Begin(SpriteSortMode.Immediate, null, null, DepthStencilState.Default, RasterizerState.CullNone, _basicEffect);
             foreach (var tileTuple in _tileList)
             {               
                 _basicEffect.World = Matrix.CreateRotationX(MathHelper.ToRadians(90)) * Matrix.CreateWorld(new Vector3(-50000 / 2, tileTuple.Item2, -50000 / 2), Vector3.Forward, Vector3.Up) * Matrix.CreateScale(0.2f);
@@ -42,31 +45,27 @@ namespace SpaceAssault.Utils
                 _basicEffect.Projection = Global.Camera.ProjectionMatrix;
                 _basicEffect.DiffuseColor = Color.LightYellow.ToVector3();
                 _basicEffect.TextureEnabled = true;
-                Global.BackgroundBatch.Begin(0, null, null, DepthStencilState.Default, RasterizerState.CullNone, _basicEffect);
-                tileTuple.Item1.Draw();
-                Global.BackgroundBatch.End();
+                tileTuple.Item1.Draw();            
             }
-            
+            Global.BackgroundBatch.End();
         }
 
         internal class Tile
         {
             private Vector2 _position;
-            private Texture2D _star;
-            private Vector2 _offset;
+            private Texture2D _texture;
 
 
-            public Tile(Vector2 position,int x , int y, Texture2D tex)
+            public Tile(Vector2 position, Texture2D tex)
             {
 
                 _position = position;
-                _star = tex;
-                _offset = new Vector2(x,y);
+                _texture = tex;
             }
 
             public void Draw()
             {
-                Global.BackgroundBatch.Draw(_star,_position + _offset, Color.White);         
+                Global.BackgroundBatch.Draw(_texture,_position , Color.White);         
             }
         }
     }
