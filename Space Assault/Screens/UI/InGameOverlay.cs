1d3ca8ede73c130cbbed8348e9f43be96887f975
gSilas <d.micheel@outlook.com>
2016-08-18 16:37:12 +0200

    removed unused files + sorted ingame overlay

--- Space Assault/Screens/UI/InGameOverlay.cs
+++ Space Assault/Screens/UI/InGameOverlay.cs
@@ -12,8 +12,9 @@ namespace SpaceAssault.Screens
         //#################################
         // Variables
         //#################################
-        List<Label> Labels = new List<Label>();
+
         private Station _station;
+        List<Label> Labels = new List<Label>();
         List<Bar> Bars = new List<Bar>();
         private UIItem _shields = new UIItem();
         private Dialog _upgradeDialog;
@@ -33,53 +34,49 @@ namespace SpaceAssault.Screens
         //#################################
         public void LoadContent(DroneBuilder droneFleet)
         {
-            /*//UI
-            Labels.Add(new Label("gamefont", "Health: ", 50, Global.GraphicsManager.PreferredBackBufferHeight - 50, Color.White));
-            Labels.Add(new Label("gamefont", "StationHealth: ", Global.GraphicsManager.PreferredBackBufferWidth - 200, Global.GraphicsManager.PreferredBackBufferHeight - 50, Color.White));
-            */
+            //UI
 
+            //Shield
             _shields.LoadContent("Images/UI/shield_ui");
-            /*Labels.Add(new Label("gamefont", "Upgrade available: ", Global.GraphicsManager.PreferredBackBufferWidth/2, 50, Color.GreenYellow));
-            Labels.Add(new Label("gamefont", "Score: ", 50, Global.GraphicsManager.PreferredBackBufferHeight - 750, Color.DarkSalmon));*/
-            //Labels.Add(new Label("gamefont", "Press B for Shop!", Global.GraphicsManager.PreferredBackBufferWidth/2, 70, Color.DarkSalmon));
+
+            //Bars
             Bars.Add(new Bar(new Rectangle(new Point(50, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 80), new Point(300, 60)), Color.Red, droneFleet.GetActiveDrone()._maxHealth));
             Bars.Add(new Bar(new Rectangle(new Point(50, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 90), new Point(300, 60)), Color.Blue, droneFleet.GetActiveDrone()._maxShield));
             Bars.Add(new Bar(new Rectangle(new Point(Global.GraphicsManager.GraphicsDevice.Viewport.Width-400, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 750), new Point(300, 60)), Color.Green, 10000));
+
+            foreach (var bar in Bars)
+            {
+                bar.LoadContent();
+            }
+
+            //Dialogs
             _upgradeDialog = new Dialog(Global.GraphicsManager.GraphicsDevice.Viewport.Width / 2-150, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 750, 32, 336, 8, false);
             _upgradeVincinityDialog = new Dialog(Global.GraphicsManager.GraphicsDevice.Viewport.Width / 2 - 150, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 702, 32, 336, 8, false);
             _scoreDialog = new Dialog(50, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 750, 32, 192, 8, false);
             _upgradeDialog.LoadContent();
             _scoreDialog.LoadContent();
             _upgradeVincinityDialog.LoadContent();
-
-            foreach (var bar in Bars)
-            {
-                bar.LoadContent();
-            }
         }
         //#################################
         // Draw
         //#################################
         public void Draw(DroneBuilder droneFleet)
         {
-
-            //Labels[1].Draw(Global.HighScorePoints);
-            _scoreDialog.Draw("Score: " + Global.HighScorePoints.ToString());
-            if (droneFleet._updatePoints > 0)
-            {
-                //Labels[0].Draw(droneFleet._updatePoints);
-                _upgradeDialog.Draw("Upgrade available: " + droneFleet._updatePoints.ToString());
-                if ((Vector3.Distance(this._station.Position, droneFleet.GetActiveDrone().Position) - GameplayScreen._stationHeight) < 150)
-                    //Labels[0].Draw();
-                    _upgradeVincinityDialog.Draw("Press B for Shop!");
-            }
-            
+            _shields.Draw(new Point(50, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 130), droneFleet._armor, new Color(1f, 1f, 1f, 0.5f));
 
             Bars[0].Draw(droneFleet.GetActiveDrone()._health, droneFleet.GetActiveDrone()._maxHealth);
             Bars[1].Draw(droneFleet.GetActiveDrone()._shield, droneFleet.GetActiveDrone()._maxShield);
             Bars[2].Draw(_station._health, 10000);
-            _shields.Draw(new Point(50, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 130), droneFleet._armor,new Color(1f,1f,1f,0.5f));
-        }
 
+            _scoreDialog.Draw("Score: " + Global.HighScorePoints.ToString());
+
+            if (droneFleet._updatePoints > 0)
+            {
+                _upgradeDialog.Draw("Upgrade available: " + droneFleet._updatePoints.ToString());
+
+                if ((Vector3.Distance(this._station.Position, droneFleet.GetActiveDrone().Position) - GameplayScreen._stationHeight) < 150)
+                    _upgradeVincinityDialog.Draw("Press B for Shop!");
+            }         
+        }
     }
 }
