038e57be0a6ae0474363005974b26a53982cf14f
iLiptho <philipp.thoms@web.de>
2016-09-08 15:49:04 +0200

    Credit Update: Complete Creditscreen to-do some more text

--- Space Assault/Screens/CreditsMenuScreen.cs
+++ Space Assault/Screens/CreditsMenuScreen.cs
@@ -1,18 +1,71 @@
-﻿namespace SpaceAssault.Screens
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SpaceAssault.Screens.UI;
+using SpaceAssault.Utils;
+
+namespace SpaceAssault.Screens
 {
     class CreditsMenuScreen : MenuScreen
     {
+        MenuEntry Programmers;
+        MenuEntry ProgramsANDLibraries;
+        MenuEntry Sounds;
+        MenuEntry SpecialThanks;
+
+        private Dialog _itemDialog;
+
+        public SortedDictionary<int, string> ShopText = new SortedDictionary<int, string>();
         // Constructor.
         public CreditsMenuScreen() : base("Credits")
         {
+
+
             // Create our menu entries.
+            Programmers =new MenuEntry("Programmers");
+            ProgramsANDLibraries=new MenuEntry("Programs and Libraries");
+            Sounds=new MenuEntry("Music and Sounds");
+            SpecialThanks=new MenuEntry("Special Thanks");
+
+            ShopText = new SortedDictionary<int, string>();
             MenuEntry back = new MenuEntry("Back");
 
             // Hook up menu event handlers.
             back.Selected += OnCancel;
 
             // Add entries to the menu.
+            MenuEntries.Add(Programmers);
+            MenuEntries.Add(ProgramsANDLibraries);
+            MenuEntries.Add(Sounds);
+            MenuEntries.Add(SpecialThanks);
             MenuEntries.Add(back);
+
+            _itemDialog = new Dialog(Global.GraphicsManager.GraphicsDevice.Viewport.Width / 2 - 150, Global.GraphicsManager.GraphicsDevice.Viewport.Height - 650, 500, 704, 8, false, false);
+
+            ShopText.Add(0, "Programmers:\n     - Philipp Thoms\n     - Dustin Boettcher\n      - Hans-Martin Wulfmeier\n     - Daniel Micheel");
+            ShopText.Add(1, "Programms:\nThis Games was created with: \nMicrosoft Visual Studio and Monogame\n\nModels:\nAll of the Models were made with 3Ds Max or Blender\n\nLibraries:\nIrrKlang");
+            ShopText.Add(2, "Music:\n   MainMenu:\n        Unrelenting by Kevin MacLeod (incompetech.com)\n        Licensed under Creative Commons: By Attribution 3.0 License\n        http://creativecommons.org/licenses/by/3.0/ \n\n   HighscoreMenu:\n        Truth of the Legend by Kevin MacLeod (incompetech.com)\n        Licensed under Creative Commons: By Attribution 3.0 License\n        http://creativecommons.org/licenses/by/3.0/ \n\n   GamePlayScreen:\n        SpaceFighterLoop by Kevin MacLeod (incompetech.com)\n        Licensed under Creative Commons: By Attribution 3.0 License\n        http://creativecommons.org/licenses/by/3.0/ \n\nSoundEffects:\n        They were made with http://www.bfxr.net/");
+            ShopText.Add(3, "Special Thanks to\n    AcaGamics\n\n    and our Testers:\n       Jan-Ole-Perschefski\n       Julia Heise\n ");
+            ShopText.Add(4, "");
+
+            _itemDialog.LoadContent();
+            _frame.LoadContent();
+
+        }
+
+        //#################################
+        // Draw
+        //#################################
+        public override void Draw(GameTime gameTime)
+        {
+            drawMenuEntries(gameTime);
+
+
+            string entry;
+            ShopText.TryGetValue(selectedEntry, out entry);
+            _itemDialog.Draw(entry);
+
+            _frame.Draw();
         }
     }
 }
