e677758108c36ff840960b9c560e4117e9984ad3
iLiptho <philipp.thoms@web.de>
2016-09-11 00:54:31 +0200

    Balance/Colors

--- Space Assault/Entities/Station.cs
+++ Space Assault/Entities/Station.cs
@@ -83,7 +83,7 @@ namespace SpaceAssault.Entities
                 if (_health < _maxhealth) _health += 100;
             }
             if (_wasDamaged == false && _shield < _maxShield)
-                _shield += _maxShield/1000;
+                _shield += _maxShield/1400;
 
             if (makeDmg != 0)
             {
