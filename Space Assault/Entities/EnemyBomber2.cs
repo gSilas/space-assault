597ae401126e0ead90fe29bb37a4a3dbace35b26
iLiptho <philipp.thoms@web.de>
2016-09-10 14:31:15 +0200

    New EnemyBomber2 and EnemyFighter3

---
+++ Space Assault/Entities/EnemyBomber2.cs
@@ -0,0 +1,49 @@
+﻿using IrrKlang;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpaceAssault.Utils;
+using SpaceAssault.Utils.Particle;
+using SpaceAssault.Utils.Particle.Settings;
+
+namespace SpaceAssault.Entities
+{
+    class EnemyBomber2 : AEnemys
+    {
+        public EnemyBomber2(Vector3 position)
+        {
+            SpawnPos = position;
+            Position = position;
+
+            _trail = new Trail(new EnemyBomberTrailSettings());
+
+            RotationMatrix = Matrix.Identity;
+
+            MoveSpeedForward = 0.8f;
+            TurnSpeed = 2.0f;
+
+            //_moveSpeedBackward = -0.5f;
+            KillMoney = 100;
+            Health = 40;
+
+            Gun = new Weapon(5000d);
+            gunMakeDmg = 500;
+        }
+
+        public override void LoadContent()
+        {
+            Model = Global.ContentManager.Load<Model>("Models/enemyship1");
+            Spheres = Collider3D.UpdateBoundingSphere(this);
+            Gun.LoadContent();
+            HitSound = Engine.AddSoundSourceFromFile("Content/Media/Effects/Objects/GetHitShips.wav", StreamMode.AutoDetect, true);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Health <= 0) IsDead = true;
+
+            Spheres = Collider3D.UpdateBoundingSphere(this);
+            //TODO: health, armor update
+        }
+    }
+}
+
---
+++ Space Assault/Entities/EnemyFighter3.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpaceAssault.Utils;
+using SpaceAssault.Utils.Particle;
+using SpaceAssault.Utils.Particle.Settings;
+using IrrKlang;
+
+namespace SpaceAssault.Entities
+{
+    class EnemyFighter3 : AEnemys
+    {
+        public EnemyFighter3(Vector3 position)
+        {
+            SpawnPos = position;
+            Position = position;
+            _trail = new Trail(new EnemyTrailSettings());
+
+            RotationMatrix = Matrix.Identity;
+            MoveSpeedForward = 1.2f;
+            TurnSpeed = 8.0f;
+
+            KillMoney = 50;
+            Health = 30;
+
+            Gun = new Weapon(600d);
+            gunMakeDmg = 10;
+        }
+
+        public override void LoadContent()
+        {
+            Model = Global.ContentManager.Load<Model>("Models/enemyship3");
+            Spheres = Collider3D.UpdateBoundingSphere(this);
+            Gun.LoadContent();
+
+            HitSound = Engine.AddSoundSourceFromFile("Content/Media/Effects/Objects/GetHitShips.wav", StreamMode.AutoDetect, true);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            Spheres = Collider3D.UpdateBoundingSphere(this);
+
+            //playing the sound
+            Vector3D curListenerPos = new Vector3D(Global.Camera.Target.X, Global.Camera.Target.Y, Global.Camera.Target.Z);
+            Engine.SetListenerPosition(curListenerPos, new Vector3D(0, 0, 1));
+
+            if (Health <= 0)
+            {
+
+                IsDead = true;
+
+            }
+        }
+    }
+}
