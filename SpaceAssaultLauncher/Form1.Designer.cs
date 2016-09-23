54ca560e06d42f5ca9ba56aee7a7be80c6f9680b
gSilas <d.micheel@outlook.com>
2016-09-23 13:17:32 +0200

    added launcher + better build structure

---
+++ SpaceAssaultLauncher/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SpaceAssaultLauncher
+{
+    partial class FormLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLauncher));
+            this.updateButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(171, 35);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(87, 19);
+            this.updateButton.TabIndex = 0;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(14, 59);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(245, 19);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Launch Space Assault";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(14, 10);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(245, 19);
+            this.progressBar.TabIndex = 2;
+            // 
+            // FormLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 94);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.updateButton);
+            this.Font = new System.Drawing.Font("ModeNine", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(289, 133);
+            this.Name = "FormLauncher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Space Assault";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+
+    }
+}
+
---
+++ SpaceAssaultLauncher/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace SpaceAssaultLauncher
+{
+    public partial class FormLauncher : System.Windows.Forms.Form
+    {
+        public FormLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void Run_Click(object sender, System.EventArgs e)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.Arguments = string.Empty;
+            start.FileName = "SpaceAssault.exe";
+            start.WindowStyle = ProcessWindowStyle.Normal;
+            start.CreateNoWindow = false;
+       
+            Process.Start(start);
+
+            this.Close();
+        }
+
+        private void Update_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+    }
+}
