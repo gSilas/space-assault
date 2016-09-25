3dad3c2d3bca1a08a5d31b5930b093bbbb615ef5
gSilas <d.micheel@outlook.com>
2016-09-25 19:56:01 +0200

    simple launcher added

--- SpaceAssaultLauncher/Form1.Designer.cs
+++ SpaceAssaultLauncher/Form1.Designer.cs
@@ -31,13 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLauncher));
             this.updateButton = new System.Windows.Forms.Button();
             this.runButton = new System.Windows.Forms.Button();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // updateButton
             // 
-            this.updateButton.Location = new System.Drawing.Point(171, 35);
+            this.updateButton.Location = new System.Drawing.Point(172, 5);
             this.updateButton.Name = "updateButton";
             this.updateButton.Size = new System.Drawing.Size(87, 19);
             this.updateButton.TabIndex = 0;
@@ -47,7 +46,7 @@
             // 
             // runButton
             // 
-            this.runButton.Location = new System.Drawing.Point(12, 59);
+            this.runButton.Location = new System.Drawing.Point(12, 30);
             this.runButton.Name = "runButton";
             this.runButton.Size = new System.Drawing.Size(247, 19);
             this.runButton.TabIndex = 1;
@@ -55,35 +54,26 @@
             this.runButton.UseVisualStyleBackColor = true;
             this.runButton.Click += new System.EventHandler(this.Run_Click);
             // 
-            // progressBar
-            // 
-            this.progressBar.Location = new System.Drawing.Point(12, 10);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(247, 19);
-            this.progressBar.TabIndex = 2;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 39);
+            this.label1.Location = new System.Drawing.Point(10, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(138, 11);
             this.label1.TabIndex = 3;
             this.label1.Text = "No Update available";
-            this.label1.TextChanged += new System.EventHandler(this.Update_Available);
             // 
             // FormLauncher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 11F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 94);
+            this.ClientSize = new System.Drawing.Size(274, 61);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.runButton);
             this.Controls.Add(this.updateButton);
             this.Font = new System.Drawing.Font("ModeNine", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(289, 133);
+            this.MaximumSize = new System.Drawing.Size(290, 100);
             this.Name = "FormLauncher";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Space Assault";
@@ -96,7 +86,6 @@
 
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Button runButton;
-        private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Label label1;
     }
 }
--- SpaceAssaultLauncher/Form1.cs
+++ SpaceAssaultLauncher/Form1.cs
@@ -1,28 +1,47 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 
 namespace SpaceAssaultLauncher
 {
     public partial class FormLauncher : System.Windows.Forms.Form
     {
+        bool updateReady;
         public FormLauncher()
         {
             InitializeComponent();
+            this.label1.ForeColor = System.Drawing.Color.Green;
+
+            string old_contents = File.ReadAllText("version");
 
             using (var client = new WebClient())
             {
                 client.Headers.Add("user-agent", "Anything");
                 client.DownloadFile(
                     "https://raw.githubusercontent.com/gSilas/space-assault-build/master/version",
-                    "version");
+                    "remoteversion");
             }
+
+            string new_contents = File.ReadAllText("remoteversion");
+
+            updateReady = !string.Equals(old_contents, new_contents);
+            if (updateReady)
+            {
+                this.label1.Text = "Update available";
+                this.label1.ForeColor = System.Drawing.Color.Red;
+                this.runButton.Visible = false;
+            }
+
         }
 
         private void Run_Click(object sender, System.EventArgs e)
         {
+            Directory.SetCurrentDirectory("data");
+
             ProcessStartInfo start = new ProcessStartInfo();
             start.Arguments = string.Empty;
-            start.FileName = "data/SpaceAssault.exe";
+            start.FileName = "SpaceAssault.exe";
             start.WindowStyle = ProcessWindowStyle.Normal;
             start.CreateNoWindow = false;
        
@@ -33,13 +52,54 @@ namespace SpaceAssaultLauncher
 
         private void Update_Click(object sender, System.EventArgs e)
         {
+            if (updateReady)
+            {
+                this.label1.Text = "Downloading!";
+                this.label1.ForeColor = System.Drawing.Color.Red;
+                updateReady = false;
 
+                try { Directory.Delete("data_old", true); }
+                catch (DirectoryNotFoundException ex) { }
+
+                try
+                {
+                    Directory.Move(@"data", @"data_old");
+                }
+                catch (DirectoryNotFoundException ex) { }
+
+                using (var client = new WebClient())
+                {
+                    client.Headers.Add("user-agent", "Anything");
+                    client.DownloadFile(
+                        "https://api.github.com/repos/gSilas/space-assault-build/zipball",
+                        "tmp.zip");
+                }
+
+                //Decompression
+                System.IO.Compression.ZipFile.ExtractToDirectory("tmp.zip", "temp");
+
+                string path = string.Empty;
+
+                var gitDir = Directory.GetDirectories("temp");
+                var p = Directory.GetDirectories(gitDir[0]);
+                path = p[0];
+
+                Directory.Move(path, "data");   
+
+                //Removing archive + versiontext
+                Directory.Delete("temp",true);
+                File.Delete("tmp.zip");
+                File.Delete("oldversion");
+
+                //adding current version
+                File.Replace("remoteversion", "version", "oldversion");
+                File.Delete("remoteversion");
+
+                //text update + start button
+                this.label1.Text = "Done! Game updated!";
+                this.label1.ForeColor = System.Drawing.Color.Green;
+                this.runButton.Visible = true;
+            }
         }
-
-        private void Update_Available(object sender, System.EventArgs e)
-        {
-            
-        }
-
     }
 }
